82f309e62328add79bd6068d45569c7c91a1e871
Oliver31Batista <ohenriquez@intellisys.com.do>
2022-08-11 14:50:33 -0400

    nodeJS server created

---
+++ backend/data.js
@@ -0,0 +1,51 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim shirt',
+            slug: 'nike-slim-shirt',
+            image: '/images/p1.jpg', //images dimensions 679 x 829
+            price: 120,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            name: 'Nike Slim pants',
+            slug: 'nike-slim-pants',
+            image: '/images/p2.jpg',
+            price: 170,
+            countInStock: 20,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            name: 'Adidas fit shirt',
+            slug: 'adidas-fit-shirt',
+            image: '/images/p3.jpg',
+            price: 90,
+            countInStock: 16,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            name: 'Adidas Slim pants',
+            slug: 'adidas-slim-pants',
+            image: '/images/p4.jpg',
+            price: 170,
+            countInStock: 20,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+
+    ]
+};
+
+export default data;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,22 @@
 import './App.css';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <Router>
-    <div>
-      <header>
-        <Link to="/">Store</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element={<ProductScreen />} />
-          <Route path="/" element={<HomeScreen />} />
-        </Routes>
-      </main>
-    </div>
+      <div>
+        <header>
+          <Link to="/">Store</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
